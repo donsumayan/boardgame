5f52386a25927a4bc9fb29d5afeedfedb9e464c5
donsumayan <don.sumayan@gmail.com>
2017-07-21 15:07:00 +0800

    Add Dama branch

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="test">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  data() {
+    return {
+      msg: 'Welcome to the test page',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import Test from '@/components/Test';
+import Dama from '@/components/dama/Board';
 
 Vue.use(Router);
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello,
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: Test,
+    },
+    {
+      path: '/dama',
+      name: 'dama',
+      component: Dama,
     },
   ],
 });
